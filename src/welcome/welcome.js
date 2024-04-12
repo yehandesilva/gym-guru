3cd05c5b288fe3fa79c515e8bf8da7620005217f
Yehan De Silva <yehan@fiberdock.ca>
2024-04-12 00:50:48 -0400

    Changed spacing

--- src/welcome/welcome.js
+++ src/welcome/welcome.js
@@ -11,27 +11,33 @@ const Welcome = () => {
 
     const [login, setLogin] = useState(true);
     return (
-        <div className='mt-8'>
-            {/* Gym Guru Logo and welcome text */}
-            <div className='flex justify-content-center'>
-                <Image src={gymGuruLogo} alt="Image" width="250"/>
-            </div>
-            <div className="flex justify-content-center text-900 text-xl font-medium mt-4">
-                {
-                    (login) ? "Welcome." : "Unlock your fitness potential today."
-                }
-            </div>
-            <div className='flex justify-content-center'>
-                <Button className='p-ripple' label={(login)? "Create an account today." : "Already have an account? Login."} onClick={() => {setLogin(!login)}} link/>
-                <Ripple />
-            </div>
-            <div className='flex justify-content-center mt-4'>
-                {
-                    (login) ?
-                        <Login setLogin={setLogin}/>
-                        :
-                        <RegisterForm setLogin={setLogin}/>
-                }
+        <div className='flex mt-8 justify-content-center align-content-center'>
+            <div className='mt-8'>
+                {/* Gym Guru Logo and welcome text */}
+                <div className='flex justify-content-center'>
+                    <Image src={gymGuruLogo} alt="Image" width="350"/>
+                </div>
+                <div className="flex justify-content-center text-900 text-4xl font-medium mt-6">
+                    {
+                        (login) ? "Welcome." : "Unlock your fitness potential today."
+                    }
+                </div>
+                <div className='flex mt-4 justify-content-center'>
+                    <Button className='p-ripple'
+                            label={(login) ? "Create an account today." : "Already have an account? Login."}
+                            onClick={() => {
+                                setLogin(!login)
+                            }} link/>
+                    <Ripple/>
+                </div>
+                <div className='flex justify-content-center mt-4'>
+                    {
+                        (login) ?
+                            <Login setLogin={setLogin}/>
+                            :
+                            <RegisterForm setLogin={setLogin}/>
+                    }
+                </div>
             </div>
         </div>
     )
